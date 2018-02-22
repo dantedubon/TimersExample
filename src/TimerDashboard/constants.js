c17bc3c9c35d6a424fda856217f06a1ed3ee0e5c
dantedubon <dante.dubon@gmail.com>
2018-02-21 23:00:32 -0600

    constants created

---
+++ src/TimerDashboard/constants.js
@@ -0,0 +1,5 @@
+export const CREATE_TIMER = 'TimerDashboard/CREATE_TIMER';
+export const UPDATE_TIMER = 'TimerDashboard/UPDATE_TIMER';
+export const DELETE_TIMER = 'TimerDashboard/DELETE_TIMER';
+export const START_TIMER = 'TimerDashboard/START_TIMER';
+export const STOP_TIMER = 'TimerDashboard/STOP_TIMER';
